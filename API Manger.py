2540bcadb39bd589121cdae91b904dbea2c6364f
Nicholas Chu <nchu999@gmail.com>
2019-06-15 16:37:46 -0500

    Refactored code

--- API Manger.py
+++ API Manger.py
@@ -2,33 +2,33 @@ import requests
 
 URL = "https://api.yelp.com/v3/businesses/search"
 HEADERS = {
-'Authorization': 'Bearer TN738I_IAGsfYxDykZL49gJMVXIxf9OfFRiB93z5s6_JMEy34C1rHW7eKERYpbYfJvELwGU17VSpL2fVAWUGxWPGKB63qh7fID38nGiwLi1nafbw_Hs_bee29EsFXXYx'
+    'Authorization': 'Bearer TN738I_IAGsfYxDykZL49gJMVXIxf9OfFRiB93z5s6_JMEy34C1rHW7eKERYpbYfJvELwGU17VSpL2fVAWUGxWPGKB63qh7fID38nGiwLi1nafbw_Hs_bee29EsFXXYx'
 }
 LIMIT = 50
 
 
-def getBusinesses(sortType):
-    file = open("WhereToGo/testfile1.txt", "w", encoding='utf-8')
+def getBusinesses(sortType: str):
+    file = open("./output.txt", "w", encoding='utf-8')
     location = "2001 E Martin Luther King Jr Blvd, Austin, TX 78702"
     # location = "Austin, TX"
     offset = 0
 
-    if sortType == 'distance':
-        PARAMS = {'limit': LIMIT, 'offset': offset,
-                  'location': location, 'sort_by': 'distance'}
-    elif sortType == 'reviewCount':
-        PARAMS = {'limit': LIMIT, 'offset': offset,
-                  'location': location, 'sort_by': 'review_count'}
-    elif sortType == 'rating':
-        PARAMS = {'limit': LIMIT, 'offset': offset,
-                  'location': location, 'sort_by': 'rating'}
-    elif sortType == 'bestMatch':
-        PARAMS = {'limit': LIMIT, 'offset': offset,
-                  'location': location, 'sort_by': 'best_match'}
-
     # to loop through 1000 businesses
     # should be range(20) for entire 1000 businesses
     for i in range(5):
+        if sortType == 'distance':
+            PARAMS = {'limit': LIMIT, 'offset': offset,
+                      'location': location, 'sort_by': 'distance'}
+        elif sortType == 'review_count':
+            PARAMS = {'limit': LIMIT, 'offset': offset,
+                      'location': location, 'sort_by': 'review_count'}
+        elif sortType == 'rating':
+            PARAMS = {'limit': LIMIT, 'offset': offset,
+                      'location': location, 'sort_by': 'rating'}
+        elif sortType == 'bestMatch':
+            PARAMS = {'limit': LIMIT, 'offset': offset,
+                      'location': location, 'sort_by': 'best_match'}
+
         r = requests.get(url=URL, headers=HEADERS, params=PARAMS)
         offset += 50
         print(str(r.status_code) + "\n")
@@ -53,4 +53,5 @@ def getBusinesses(sortType):
     file.close
 
 
-getBusinesses('rating')
+sortingMethod = input("Enter a sorting method:")
+getBusinesses(sortingMethod)
